32c1916f15fa2976aacf7c7f05b72be16b4dc27d
ZiP <thezip33@gmail.com>
2018-11-26 23:31:25 +0200

    Это мой первый комит

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let persone = {
+    name: "Egor",
+    age: 25,
+    isMarried: false
+}
+// alert("hi, ass!");
+// console.log(persone.name);
+// console.log(persone['name']);
+
+//let ans = confirm("Are U here?");
+//console.log(ans);
+// let ans = prompt("are u 18?", "yes");
+// console.log(typeof(ans));
+
+let dec = 5,
+    inc = 5;
+
+console.log(dec++, dec);
+console.log('2' === 2);
